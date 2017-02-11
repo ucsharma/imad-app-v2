7c23217463b91bfc56c78ca4da1fc51f343e8f56
Umesh Sharma <ucsinc@gmail.com>
2017-02-11 23:17:43 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,19 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
+app.get('/article-one', function (req, res) {
+  res.send('Article One has been requested and will be server here');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article Two has been requested and will be server here');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Article Three has been requested and will be server here');
+});
+
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
