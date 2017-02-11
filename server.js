3feb4bb125d01cbfd7eb29df3fa9449044c0543c
Umesh Sharma <ucsinc@gmail.com>
2017-02-11 23:46:23 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('Article Two has been requested and will be server here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('Article Three has been requested and will be server here');
+ res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
